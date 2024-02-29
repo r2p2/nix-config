23cbeb1236d80a1734121cccaa357cb240336d92
Robert Peters <ropeters@pm.me>
2024-02-29 22:00:17 +0100

    Remove vbox guest config from vbox-test-env/config
    The hardware configuration already contained vbox guest enable.

--- hosts/vbox-test-env/configuration.nix
+++ hosts/vbox-test-env/configuration.nix
@@ -74,8 +74,5 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.x11 = true;
 }
 
